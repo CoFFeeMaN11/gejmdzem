6084d37ca9a0e36902dc71b6941f5cd88da2a872
GroveW <qswdw3ed3@gmail.com>
2018-09-21 22:03:48 +0200

    building scritable object

---
+++ Assets/_scripts/BuildingScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Building", order = 1)]
+
+public class BuildingScript : ScriptableObject
+{
+    [SerializeField]
+    private int health;
+    [SerializeField]
+    private Sprite sprite;
+    [SerializeField]
+    private int price;
+    [SerializeField]
+    [TextArea]
+    private string description;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void Repair()
+    {
+
+    }
+
+    void GetUpgrades()
+    {
+
+    }
+
+    void Used()
+    {
+
+    }
+}
---
+++ Assets/_scripts/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    public enum EnemyType
+    {
+        small,
+        medium,
+        big
+    }
+
+    public EnemyType Type;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
