b170ea3fb4e4d86f60514625da4b2e8a93070b91
GroveW <qswdw3ed3@gmail.com>
2018-09-22 07:07:46 +0200

    prefabs

--- Assets/_scripts/PlayerScript.cs
+++ Assets/_scripts/PlayerScript.cs
@@ -5,19 +5,25 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour {
 
-    public static int Gold;
+    public static int Gold = 3000;
     public static int Wood = 3000;
-    public static int Stone;
+    public static int Stone = 3000;
 
     public Text WoodText;
+    public Text GoldText;
+    public Text StoneText;
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         WoodText.text = Wood.ToString();
-	}
+        GoldText.text = Wood.ToString();
+        StoneText.text = Wood.ToString();
+    }
 }
