5dbc0b416a22eafb024c529209e453a45cb1d1b7
Krzysztof Zawadzki <a1ml0ck000@gmail.com>
2018-09-23 11:43:44 +0200

    no message

---
+++ Assets/_scripts/BuildingItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingItem : MonoBehaviour {
+    public Image avatar;
+    public Text nameText, woodText, stoneText, goldText;
+    public Button button;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
